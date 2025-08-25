3264740dca0007da903764d042ff9cfc43d79b9b
Hunor Takacs <tkcshnr@gmail.com>
2025-08-25 17:54:15 +0200

    carousel scroll fix

--- src/lib/components/TeacherCarousel.svelte
+++ src/lib/components/TeacherCarousel.svelte
@@ -1,33 +1,49 @@
-<script>
+<script lang="ts">
 	import teachers from '$lib/teachers.json';
 	import TeacherCard from './TeacherCard.svelte';
+
+	let carouselElement: HTMLDivElement;
+	const scrollAmount = 10;
+
+	function scrollPrev() {
+		carouselElement.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+	}
+
+	function scrollNext() {
+		carouselElement.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+	}
 </script>
 
-<div class="indicator mt-4 w-full max-w-4xl flex justify-center">
-	<span class="indicator-item indicator-center badge badge-lg badge-info">Tanáraink</span>
-	<div
-		class="absolute left-0 h-full w-6 z-10 rounded-l-3xl bg-gradient-to-r from-base-300 to-transparent hidden lg:block"
-	></div>
-	<div class="carousel carousel-center gap-6 bg-base-300 p-6 lg:rounded-3xl relative">
-		{#each teachers as teacher}
-			<TeacherCard
-				name={teacher.name}
-				imageUrl={teacher.imageUrl}
-				subjects={teacher.subjects}
-				description={teacher.description}
-			/>
-		{/each}
+<div class="flex flex-col items-center gap-2 w-full">
+	<div class="indicator mt-4 w-full max-w-4xl flex justify-center">
+		<span class="indicator-item indicator-center badge badge-lg badge-info">Tanáraink</span>
+		<div
+			class="absolute left-0 h-full w-6 z-10 rounded-l-3xl bg-gradient-to-r from-base-300 to-transparent hidden lg:block"
+		></div>
+		<div
+			class="carousel carousel-center w-full gap-6 bg-base-300 p-6 lg:rounded-3xl relative"
+			bind:this={carouselElement}
+		>
+			{#each teachers as teacher}
+				<TeacherCard
+					name={teacher.name}
+					imageUrl={teacher.imageUrl}
+					subjects={teacher.subjects}
+					description={teacher.description}
+				/>
+			{/each}
+		</div>
+		<div
+			class="absolute right-0 h-full w-6 z-10 rounded-r-3xl bg-gradient-to-r from-transparent to-base-300 hidden lg:block"
+		></div>
 	</div>
-	<div
-		class="absolute right-0 h-full w-6 z-10 rounded-r-3xl bg-gradient-to-r from-transparent to-base-300 hidden lg:block"
-	></div>
-</div>
 
-<div class="join">
-	<button class="btn join-item btn-square" aria-label="prev">
-		<i class="fa-solid fa-arrow-left"></i>
-	</button>
-	<button class="btn join-item btn-square" aria-label="next">
-		<i class="fa-solid fa-arrow-right"></i>
-	</button>
+	<div class="join">
+		<button class="btn join-item btn-square" aria-label="prev" onclick={scrollPrev}>
+			<i class="fa-solid fa-arrow-left"></i>
+		</button>
+		<button class="btn join-item btn-square" aria-label="next" onclick={scrollNext}>
+			<i class="fa-solid fa-arrow-right"></i>
+		</button>
+	</div>
 </div>
