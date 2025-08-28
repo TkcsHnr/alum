173d126477fb2a922d85024a28b8735cfadba2ad
Hunor Takacs <tkcshnr@gmail.com>
2025-08-28 16:23:46 +0200

    basic contact page

--- src/routes/kapcsolat/+page.svelte
+++ src/routes/kapcsolat/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
 	import type { PageProps } from './$types';
+	import EmailCard from './EmailCard.svelte';
 
 	let { data }: PageProps = $props();
 </script>
 
 <Container>
 	<h1 class="text-2xl font-bold mb-4 mx-auto">Kapcsolat</h1>
+
+	<div class="grid md:grid-cols-2 gap-4">
+		<EmailCard title="Általános információk" description="" email="info@alumoktatas.hu" />
+		<EmailCard title="Felvételi" description="" email="felveteli@alumoktatas.hu" />
+		<EmailCard title="Érettségi" description="" email="erettsegi@alumoktatas.hu" />
+		<EmailCard title="Korrepetálás" description="" email="korrepetalas@alumoktatas.hu" />
+	</div>
 </Container>
---
+++ src/routes/kapcsolat/EmailCard.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { title, description, email } = $props();
+</script>
+
+<div class="card bg-base-200 border border-base-content/15 shadow-sm w-full max-w-md">
+	<div class="card-body p-4">
+		<h2 class="card-title">{title}</h2>
+		<p>
+			{#if description}
+				{description}
+			{:else}
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi necessitatibus ad cumque
+				blanditiis perferendis expedita alias nostrum enim possimus illo.
+			{/if}
+		</p>
+		<div class="card-actions justify-end">
+			<a class="btn btn-primary" href="mailto:{email}">{email}</a>
+		</div>
+	</div>
+</div>
